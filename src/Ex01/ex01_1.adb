885d861d9185f4cf4b2892c44b02dfc528477254
Hy-gon <mickael.lemieux@outlook.com>
2025-06-10 20:44:32 +0200

    :tada: Init ada exercice project

---
+++ src/Ex01/ex01_1.adb
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure ex01_1 is
+begin
+   Put_Line("Ceci est l'exercice 1.1");
+end ex01_1;
---
+++ src/main.adb
@@ -0,0 +1,11 @@
+with ex01_1;
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("");
+   Put_Line ("");
+   ex01_1;
+   Put_Line ("");
+   Put_Line ("");
+end Main;
