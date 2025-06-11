27ec1a0be31c944e9222879c7f1407afd009af04
Hy-gon <mickael.lemieux@outlook.com>
2025-06-11 23:22:11 +0200

    :zap: Modify the print for each exercices

--- src/main.adb
+++ src/main.adb
@@ -1,9 +1,20 @@
-with ex01_2;
+with ex01_1; with ex01_2;
+with ex01_3;
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
+   procedure Afficher_Exercice (Nom : String; Proc : access procedure) is
+   begin
+      Put_Line ("================");
+      Put_Line ("Exercice : " & Nom);
+      Put_Line ("================");
+      Proc.all;
+      New_Line;
+   end Afficher_Exercice;
+
 begin
    New_Line;
-   ex01_2;
-   New_Line;
+   Afficher_Exercice ("01.1", ex01_1'Access);
+   Afficher_Exercice ("01.2", ex01_2'Access);
+   Afficher_Exercice ("01.3", ex01_3'Access);
 end Main;
