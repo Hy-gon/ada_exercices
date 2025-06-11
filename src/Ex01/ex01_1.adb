4e967104aeb5c760e2fe925d1bd9a85fe8bff6ec
Hy-gon <mickael.lemieux@outlook.com>
2025-06-11 23:20:33 +0200

    :rewind: Removing print exercice

--- src/Ex01/ex01_1.adb
+++ src/Ex01/ex01_1.adb
@@ -2,8 +2,5 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure ex01_1 is
 begin
-   Put_Line ("Exercice 01.1");
-   Put_Line ("-------------");
-
    Put_Line ("Bonjour Ada !");
 end ex01_1;
--- src/Ex01/ex01_2.adb
+++ src/Ex01/ex01_2.adb
@@ -4,11 +4,8 @@ procedure ex01_2 is
    Name : constant String := "Hy-gon";
    Age  : Integer;
 begin
-   Put_Line ("Exercice 01.2");
-   Put_Line ("-------------");
-
    Age := 29;
 
    Put_Line (Name);
-   Put_Line ("Je m'appelle " & Name & " et j'ai " & Age'Image & "ans");
+   Put_Line ("Je m'appelle " & Name & " et j'ai" & Age'Image & "ans");
 end ex01_2;
