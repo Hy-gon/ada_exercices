5ed636358866b3cbcf5a728ebd9bdc925dedab7e
Hy-gon <mickael.lemieux@outlook.com>
2025-06-12 20:55:07 +0200

    :white_check_mark: Adding Ex01_4

--- src/main.adb
+++ src/main.adb
@@ -1,5 +1,5 @@
 with ex01_1; with ex01_2;
-with ex01_3;
+with ex01_3; with ex01_4;
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
@@ -17,4 +17,7 @@ begin
    Afficher_Exercice ("01.1", ex01_1'Access);
    Afficher_Exercice ("01.2", ex01_2'Access);
    Afficher_Exercice ("01.3", ex01_3'Access);
+   Afficher_Exercice ("01.4", ex01_4'Access);
+   Put_Line ("Fin des exercices.");
+   New_Line;
 end Main;
